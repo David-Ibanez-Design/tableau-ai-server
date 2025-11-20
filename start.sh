0acf13d8ede2bd1939d831f62fc286b57045e736
David-Ibanez-Design <dibanez@rocketmail.com>
2025-11-20 21:49:46 +0900

    Updated password file path

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # 1. Create the password file using Environment Variables
 # We will set TABPY_USERNAME and TABPY_PASSWORD in the Render dashboard later.
-tabpy-user add -u $TABPY_USERNAME -p $TABPY_PASSWORD -f /etc/secrets/passwd.txt
+tabpy-user add -u $TABPY_USERNAME -p $TABPY_PASSWORD -f /opt/render/project/src/passwd.txt
 
 # 2. Start TabPy using the port Render assigns ($PORT)
 export TABPY_PORT=$PORT
